31ba70a73b4ac7e03ff22a05807ecb6d0ae5c9e1
jjhwnn <jeahwan174@gmail.com>
2022-03-29 12:38:29 +0900

    Update UDto, TDto and Create CDto

---
+++ ToDoList/src/main/java/com/todo/dto/CDto.java
@@ -0,0 +1,27 @@
+package com.todo.dto;
+
+public class CDto {
+	
+	private String cname;
+	
+	public CDto() {
+
+	}
+
+	public CDto(String cname) {
+		super();
+		this.cname = cname;
+	}
+
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	
+	
+}
